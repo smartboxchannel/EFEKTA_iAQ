7770efd7af9bb9952dc9ecf7b0e9d68a37623e69
Andrew Lamchenko <31867646+smartboxchannel@users.noreply.github.com>
2023-06-27 20:03:25 +0300

    Add files via upload

---
+++ converter/EFEKTA_iAQ3.js
@@ -0,0 +1,225 @@
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const tz = require('zigbee-herdsman-converters/converters/toZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const extend = require('zigbee-herdsman-converters/lib/extend');
+const constants = require('zigbee-herdsman-converters/lib/constants');
+const e = exposes.presets;
+const ea = exposes.access;
+const {calibrateAndPrecisionRoundOptions} = require('zigbee-herdsman-converters/lib/utils');
+
+async function onEventSetLocalTime(type, data, device) {
+ 
+    if (data.type === 'attributeReport' && data.cluster === 'genTime') {
+	    try {	
+		    const endpoint = device.getEndpoint(1);
+		    const time = Math.round((((new Date()).getTime() - constants.OneJanuary2000) / 1000) + (((new Date()).getTimezoneOffset() * -1) * 60));
+            await endpoint.write('genTime', {time: time});
+        }catch (error) {
+            // endpoint.write can throw an error which needs to
+            // be caught or the zigbee-herdsman may crash
+            // Debug message is handled in the zigbee-herdsman
+        }
+    }
+}
+
+
+const tzLocal = {
+    node_config: {
+        key: ['report_delay'],
+        convertSet: async (entity, key, rawValue, meta) => {
+			const endpoint = meta.device.getEndpoint(1);
+            const lookup = {'OFF': 0x00, 'ON': 0x01};
+            const value = lookup.hasOwnProperty(rawValue) ? lookup[rawValue] : parseInt(rawValue, 10);
+            const payloads = {
+                report_delay: ['genPowerCfg', {0x0201: {value, type: 0x21}}],
+            };
+            await endpoint.write(payloads[key][0], payloads[key][1]);
+            return {
+                state: {[key]: rawValue},
+            };
+        },
+    },
+	co2_config: {
+        key: ['auto_brightness', 'onoff_brightness', 'forced_recalibration', 'factory_reset_co2', 'long_chart_period', 'long_chart_period2', 'set_altitude', 'manual_forced_recalibration'],
+        convertSet: async (entity, key, rawValue, meta) => {
+			const endpoint = meta.device.getEndpoint(1);
+            const lookup = {'OFF': 0x00, 'ON': 0x01};
+            const value = lookup.hasOwnProperty(rawValue) ? lookup[rawValue] : parseInt(rawValue, 10);
+            const payloads = {
+                auto_brightness: ['msCO2', {0x0203: {value, type: 0x10}}],
+				onoff_brightness: ['msCO2', {0x0401: {value, type: 0x10}}],
+                forced_recalibration: ['msCO2', {0x0202: {value, type: 0x10}}],
+                factory_reset_co2: ['msCO2', {0x0206: {value, type: 0x10}}],
+				long_chart_period: ['msCO2', {0x0204: {value, type: 0x10}}],
+				long_chart_period2: ['msCO2', {0x0404: {value, type: 0x10}}],
+                set_altitude: ['msCO2', {0x0205: {value, type: 0x21}}],
+                manual_forced_recalibration: ['msCO2', {0x0207: {value, type: 0x21}}],
+            };
+            await endpoint.write(payloads[key][0], payloads[key][1]);
+            return {
+                state: {[key]: rawValue},
+            };
+        },
+    },
+	
+	co2_gasstat_config: {
+        key: ['high_gas', 'low_gas', 'enable_gas', 'invert_logic_gas'],
+        convertSet: async (entity, key, rawValue, meta) => {
+			const endpoint = meta.device.getEndpoint(1);
+            const lookup = {'OFF': 0x00, 'ON': 0x01};
+            const value = lookup.hasOwnProperty(rawValue) ? lookup[rawValue] : parseInt(rawValue, 10);
+            const payloads = {
+                high_gas: ['msCO2', {0x0221: {value, type: 0x21}}],
+                low_gas: ['msCO2', {0x0222: {value, type: 0x21}}],
+				enable_gas: ['msCO2', {0x0220: {value, type: 0x10}}],
+				invert_logic_gas: ['msCO2', {0x0225: {value, type: 0x10}}],
+            };
+            await endpoint.write(payloads[key][0], payloads[key][1]);
+            return {
+                state: {[key]: rawValue},
+            };
+        },
+    },
+};
+
+const fzLocal = {
+	local_time: {
+        cluster: 'genTime',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            return {local_time: msg.data.localTime};
+        },
+    },
+    node_config: {
+        cluster: 'genPowerCfg',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            const result = {};
+            if (msg.data.hasOwnProperty(0x0201)) {
+                result.report_delay = msg.data[0x0201];
+            }
+            return result;
+        },
+    },
+	co2: {
+        cluster: 'msCO2',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+			if (msg.data.hasOwnProperty('measuredValue')) {
+				return {co2: Math.round(msg.data.measuredValue * 1000000)};
+			}
+        },
+    },
+	air_quality: {
+        cluster: 'genAnalogInput',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+			const result = {};
+            if (msg.data.hasOwnProperty(0x0065)) {
+                result.voc_index = msg.data[0x0065];
+            }
+			if (msg.data.hasOwnProperty('presentValue')) {
+			    result.voc_raw_data = msg.data.presentValue;
+			}
+			return result;
+        },
+    },
+	co2_config: {
+        cluster: 'msCO2',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            const result = {};
+            if (msg.data.hasOwnProperty(0x0203)) {
+                result.auto_brightness = ['OFF', 'ON'][msg.data[0x0203]];
+            }
+			if (msg.data.hasOwnProperty(0x0401)) {
+                result.onoff_brightness = ['OFF', 'ON'][msg.data[0x0401]];
+            }
+            if (msg.data.hasOwnProperty(0x0202)) {
+                result.forced_recalibration = ['OFF', 'ON'][msg.data[0x0202]];
+            }
+            if (msg.data.hasOwnProperty(0x0206)) {
+                result.factory_reset_co2 = ['OFF', 'ON'][msg.data[0x0206]];
+            }
+			if (msg.data.hasOwnProperty(0x0204)) {
+                result.long_chart_period = ['OFF', 'ON'][msg.data[0x0204]];
+            }
+			if (msg.data.hasOwnProperty(0x0404)) {
+                result.long_chart_period2 = ['OFF', 'ON'][msg.data[0x0404]];
+            }
+            if (msg.data.hasOwnProperty(0x0205)) {
+                result.set_altitude = msg.data[0x0205];
+            }
+            if (msg.data.hasOwnProperty(0x0207)) {
+                result.manual_forced_recalibration = msg.data[0x0207];
+            }
+            return result;
+        },
+    },
+	co2_gasstat_config: {
+        cluster: 'msCO2',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options, meta) => {
+            const result = {};
+            if (msg.data.hasOwnProperty(0x0221)) {
+                result.high_gas = msg.data[0x0221];
+            }
+			if (msg.data.hasOwnProperty(0x0222)) {
+                result.low_gas = msg.data[0x0222];
+            }
+            if (msg.data.hasOwnProperty(0x0220)) {
+                result.enable_gas = ['OFF', 'ON'][msg.data[0x0220]];
+            }
+			if (msg.data.hasOwnProperty(0x0225)) {
+                result.invert_logic_gas = ['OFF', 'ON'][msg.data[0x0225]];
+            }
+            return result;
+        },
+    },
+};
+
+const definition = {
+        zigbeeModel: ['EFEKTA_iAQ3'],
+        model: 'EFEKTA_iAQ3',
+        vendor: 'Custom devices (DiY)',
+        description: '[CO2 Monitor with IPS TFT Display, outdoor temperature and humidity, date and time.](http://efektalab.com/iAQ)',
+        fromZigbee: [fz.temperature, fz.humidity, fz.illuminance, fzLocal.co2, fzLocal.air_quality, fzLocal.co2_config, fzLocal.local_time, fzLocal.co2_gasstat_config],
+        toZigbee: [tz.factory_reset, tzLocal.co2_config, tzLocal.co2_gasstat_config],
+		onEvent: onEventSetLocalTime,
+        configure: async (device, coordinatorEndpoint, logger) => {
+            const endpoint = device.getEndpoint(1);
+			const endpoint2 = device.getEndpoint(2);
+            await reporting.bind(endpoint, coordinatorEndpoint, [
+                'genTime', 'msTemperatureMeasurement', 'msRelativeHumidity', 'msCO2']);
+		    await reporting.bind(endpoint2, coordinatorEndpoint, ['msIlluminanceMeasurement', 'genAnalogInput']);
+        },
+        exposes: [e.co2(), e.temperature(), e.humidity(), e.illuminance_lux(), e.illuminance(),
+		    exposes.numeric('voc_index', ea.STATE).withUnit('VOC Index points').withDescription('VOC INDEX'),
+            exposes.numeric('voc_raw_data', ea.STATE).withUnit('ticks').withDescription('SRAW_VOC, digital raw value'),
+            exposes.binary('auto_brightness', ea.STATE_SET, 'ON', 'OFF')
+			    .withDescription('Enable or Disable Auto Brightness of the Display'),
+		    exposes.binary('onoff_brightness', ea.STATE_SET, 'ON', 'OFF')
+			    .withDescription('Complete shutdown of the backlight at night'),
+			exposes.binary('long_chart_period', ea.STATE_SET, 'ON', 'OFF')
+			    .withDescription('The period of plotting the CO2 level(OFF - 1H | ON - 24H)'),
+			exposes.binary('long_chart_period2', ea.STATE_SET, 'ON', 'OFF')
+			    .withDescription('The period of plotting the VOC Index points(OFF - 1H | ON - 24H)'),
+			exposes.numeric('set_altitude', ea.STATE_SET).withUnit('meters')
+			    .withDescription('Setting the altitude above sea level (for high accuracy of the CO2 sensor)')
+                .withValueMin(0).withValueMax(3000),
+			exposes.binary('forced_recalibration', ea.STATE_SET, 'ON', 'OFF')
+			    .withDescription('Start FRC (Perform Forced Recalibration of the CO2 Sensor)'),
+			exposes.binary('factory_reset_co2', ea.STATE_SET, 'ON', 'OFF').withDescription('Factory Reset CO2 sensor'),
+            exposes.numeric('manual_forced_recalibration', ea.STATE_SET).withUnit('ppm')
+			    .withDescription('Start Manual FRC (Perform Forced Recalibration of the CO2 Sensor)')
+                .withValueMin(0).withValueMax(5000),
+		    exposes.binary('enable_gas', ea.STATE_SET, 'ON', 'OFF').withDescription('Enable CO2 Gas Control'),
+			exposes.binary('invert_logic_gas', ea.STATE_SET, 'ON', 'OFF').withDescription('Enable invert logic CO2 Gas Control'),
+            exposes.numeric('high_gas', ea.STATE_SET).withUnit('ppm').withDescription('Setting High CO2 Gas Border')
+                .withValueMin(400).withValueMax(5000),
+            exposes.numeric('low_gas', ea.STATE_SET).withUnit('ppm').withDescription('Setting Low CO2 Gas Border')
+                .withValueMin(400).withValueMax(5000)],
+};
+
+module.exports = definition;
